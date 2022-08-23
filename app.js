4619a6f94cb2c551e9939f5366633b99d1606233
astridsen <103685822+astridsen@users.noreply.github.com>
2022-08-23 22:57:00 +0200

    Fin route api sauce

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
 const express = require('express');
+
+const app = express();
+
 const mongoose = require('mongoose');
+
+const sauceRoutes = require('./routes/sauce');
 
 const userRoutes = require('./routes/user');
 
-mongoose.connect('mongodb+srv://astridsen:B779tTvbkjD0QcrC@cluster0.1jpgm8m.mongodb.net/?retryWrites=true&w=majority',
-{ useNewUrlParser: true,
-  useUnifiedTopology: true })
-.then(() => console.log('Connexion à MongoDB réussie !'))
-.catch(() => console.log('Connexion à MongoDB échouée !'));
+const path = require('path');
 
-const app = express();
+mongoose.connect('mongodb+srv://astridsen:vTaIrNurmjXOgfIx@cluster0.1jpgm8m.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Successful connection to MongoDB !'))
+  .catch(() => console.log('Connection to MongoDB failed !'));
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
+app.use('/api/sauces', sauceRoutes);
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
-
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcrypt');
-const User = require('../models/User');
+
+const User = require('../models/user');
+
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -9,7 +12,7 @@ exports.signup = (req, res, next) => {
           password: hash
         });
         user.save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+          .then(() => res.status(201).json({ message: 'Created user !' }))
           .catch(error => res.status(400).json({ error }));
       })
       .catch(error => res.status(500).json({ error }));
@@ -19,19 +22,23 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
-                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+                return res.status(401).json({ error: 'User no found !' });
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                        return res.status(401).json({ error: 'Invalid password !' });
                     }
                     res.status(200).json({
                         userId: user._id,
-                        token: 'TOKEN'
+                        token: jwt.sign(
+                            { userId: user._id },
+                            'RANDOM_TOKEN_SECRET',
+                            { expiresIn: '24h' }
+                        )
                     });
                 })
                 .catch(error => res.status(500).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
- };
+};
