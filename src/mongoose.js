ba1b7d3f894eeb3eacf3c53ed3bc74d435aacc18
astridsen <103685822+astridsen@users.noreply.github.com>
2022-08-26 15:10:42 +0200

    Optimisation architecture

---
+++ src/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MongoDB_URI,
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Successful connection to MongoDB !'))
+    .catch(() => console.log('Connection to MongoDB failed !'));
